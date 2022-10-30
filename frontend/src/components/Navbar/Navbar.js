df2c027c52d5794c733a68381df6b25ba5eaa91a
abhigyanhedau2 <93371184+abhigyanhedau2@users.noreply.github.com>
2022-10-30 19:07:50 +0530

    Navbar Completed

--- frontend/src/components/Navbar/Navbar.js
+++ frontend/src/components/Navbar/Navbar.js
@@ -1,10 +1,29 @@
 import React from 'react';
+import { useLocation, Link } from 'react-router-dom';
 
-import './Navbar.css';
+import classes from './Navbar.module.css';
 
 const Navbar = () => {
+
+    const path = useLocation().pathname;
+
     return (
-        <div className='text'>Navbar</div>
+        <nav className={classes.navbar}>
+            <div className={classes.navbar__right}>
+                <div className={classes.navbar__navlinks}>
+                    <ul>
+                        <li className={path === '/' ? classes.active : ''}><Link to='/'>Home</Link></li>
+                        <li className={path === '/about' ? classes.active : ''}><Link to='/about'>About</Link></li>
+                        <li className={path === '/contact' ? classes.active : ''}><Link to='/contact'>Contact</Link></li>
+                    </ul>
+                </div>
+                <div className={classes.navbar__actions}>
+                    <button>Login/Signup</button>
+                    {/* <button>Cart <i class="fa-solid fa-cart-shopping"></i></button>
+                    <button>Logout</button> */}
+                </div>
+            </div>
+        </nav>
     )
 };
 
