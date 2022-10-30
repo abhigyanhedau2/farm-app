3a60fe19345bde7a9a2360eb021841e28c1f62e1
abhigyanhedau2 <93371184+abhigyanhedau2@users.noreply.github.com>
2022-10-30 19:53:16 +0530

    Header Completed

--- frontend/src/components/Header/Header.js
+++ frontend/src/components/Header/Header.js
@@ -1,11 +1,22 @@
 import React from 'react';
 
+import logo from '../../assets/logo.png';
+
 import classes from './Header.module.css';
 
 const Header = () => {
     return (
         <div className={classes.header}>
-            
+            <div className={classes.header__logo}>
+                <img src={logo} alt="" />
+            </div>
+            <div className={classes['header__main-content']}>
+                <h1 className={classes['heading-primary']}>
+                    <span className={classes['heading-primary--main']}>Birch Wood Ranch</span>
+                    <span className={classes['heading-primary--sub']}>Farm Produce & More</span>
+                </h1>
+                <button className={classes.btn + classes['btn--white'] + classes['btn--animated']}>Explore</button>
+            </div>
         </div>
     )
 };
